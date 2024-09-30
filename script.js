acca3368aa5f36db1a6729714208e67e62ca0d60
chim7a <chimalawrence11@gmail.com>
2024-09-30 18:33:34 +0100

    Counter function

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
+const itemQuantity = document.getElementById("item-quantity");
+const addBtn = document.getElementById("add-btn");
+addBtn.addEventListener("click", addItem);
+const minusBtn = document.getElementById("minus-btn");
+minusBtn.addEventListener("click", minusItem);
+let items = 1;
 
+// Add quantity of items
+function addItem() {
+  if (addBtn) {
+    items++;
+    itemQuantity.innerText = `${items}`;
+  }
+}
+addItem();
+
+// Minus quantity of items
+
+function minusItem() {
+  if (items > 0) {
+    items--;
+    itemQuantity.innerText = `${items}`;
+  }
+}
+
+minusItem();
+
+// DOMContentLoaded
+
+// if (document.readyState === "loading") {
+//   document.addEventListener("DOMContentLoaded", addItem);
+// } else {
+//   addItem();
+// }
