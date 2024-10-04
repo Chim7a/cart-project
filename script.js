2d1d5fd0f7ce2989eec073ae08cda84cdece54ba
chim7a <chimalawrence11@gmail.com>
2024-10-04 21:42:04 +0100

    Array OBJ created and displayed on screen

--- script.js
+++ script.js
@@ -1,34 +1,94 @@
-const itemQuantity = document.getElementById("item-quantity");
-const addBtn = document.getElementById("add-btn");
-addBtn.addEventListener("click", addItem);
-const minusBtn = document.getElementById("minus-btn");
-minusBtn.addEventListener("click", minusItem);
-let items = 1;
+const CART_CONTAINER = document.querySelector(".cart-container");
+console.log(CART_CONTAINER);
 
-// Add quantity of items
-function addItem() {
-  if (addBtn) {
-    items++;
-    itemQuantity.innerText = `${items}`;
-  }
+let CART = [
+  {
+    id: 1,
+    name: "Television",
+    brand: "LG",
+    price: 5620,
+  },
+  {
+    id: 44,
+    name: "Sneakers",
+    brand: "Nike",
+    price: 80,
+  },
+  {
+    id: 11,
+    name: "Kettle",
+    brand: "Sharp",
+    price: 70,
+  },
+  {
+    id: 33,
+    name: "Iphone 16",
+    brand: "Apple",
+    price: 999,
+  },
+  {
+    id: 19,
+    name: "Camera",
+    brand: "Sony",
+    price: 2000,
+  },
+];
+
+function displayCart(params) {
+  let cartElement = [];
+
+  CART.map((eachCartItem) => {
+    CART_CONTAINER.innerHTML += `
+  <div class="cart-item">
+        <!-- Product image and name -->
+        <div class="product">
+          <img
+            src="https://images.unsplash.com/photo-1601944179066-29786cb9d32a?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt=""
+            width="70px"
+            height="70px"
+          />
+          <div class="product-name">
+            <h3>${eachCartItem.name}</h3>
+            <p>${eachCartItem.brand}</p>
+          </div>
+        </div>
+
+        <!-- Product price and Quantity -->
+
+        <div class="product-price">
+          <!-- Price of item -->
+          <p class="item-price">${eachCartItem.price}</p>
+
+          <!-- Delete item -->
+          <button class="delete-btn">
+            Delete
+          </button>
+
+          <!-- Product counter -->
+          <div class="product-counter">
+          
+            <!-- Minus button -->
+            <button class="minus-btn">
+              -
+            </button>
+
+            <!-- Item Quantity -->
+            <p class="item-quantity"></p>
+
+            <!-- Add item Button -->
+            <button class="add-btn">
+              +
+            </button>
+          </div>
+          <!-- Total Price  -->
+
+          <p class="total-price"></p>
+        </div>
+      </div>
+  `;
+  });
+  console.log(CART);
 }
-addItem();
 
-// Minus quantity of items
-
-function minusItem() {
-  if (items > 0) {
-    items--;
-    itemQuantity.innerText = `${items}`;
-  }
-}
-
-minusItem();
-
-// DOMContentLoaded
-
-// if (document.readyState === "loading") {
-//   document.addEventListener("DOMContentLoaded", addItem);
-// } else {
-//   addItem();
-// }
+displayCart();
